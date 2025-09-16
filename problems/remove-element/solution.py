f114cf8564e196dae1cfab0334d2d4ce37ee304a
tsecret <timichfull@gmail.com>
2025-09-16 18:07:45 +0200

    pytest

--- problems/remove-element/solution.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-# URL: https://leetcode.com/problems/remove-element
-# topics: array, two-pointers
-
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        1
